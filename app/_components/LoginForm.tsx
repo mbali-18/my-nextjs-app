7b2ec7bc22ac559b78b95b3020728e4a1f6c8678
mbali-18 <faithfulservent18@gmail.com>
2026-02-04 12:47:43 +0200

    Auth server action

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { use } from "react";
+import { loginAction } from "../actions/auth";
 
 const LoginForm = () => {
   return (
-    <form className="space-x-4">
+    <form action={loginAction} className="space-x-4">
       <div>
         <label className="block text-sm font-medium text-gray-700">Email</label>
         <input
--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -10,17 +10,25 @@ type UserType = {
 
 const API_URL = "http://localhost:3001/users";
 
-export const login = async (formData: FormData) => {
+export const loginAction = async (formData: FormData) => {
+    console.log("formData:", formData)
     try {
         const response = await axios.get(`${API_URL}/users?email=${formData.get("email")}&password=${formData.get("password")}`);
-        const user: UserType 
-    } catch (error) {
+        const user: UserType = response.data[0]; 
+        if (!user) {
+            throw new Error("Invalid Credentials");
+            //set user in the cookies 
+            
 
+        } 
+        redirect('/contact');
+    } catch (error) {
+        throw new Error("Login failed");
     }
 
 };
 
-export const logout = () => {
+export const logout = async () => {
  redirect ('/login');
 };
 
